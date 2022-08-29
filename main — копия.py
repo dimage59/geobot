ca5957c1c24d46c78f78a54379676188d368382a
dmitry zmeev <dzmeev59@gmail.com>
2022-08-29 09:32:04 +0500

    third commit

--- main — копия.py
+++ main — копия.py
@@ -1,7 +1,25 @@
 import openpyxl
+a=("П")
 wookbook = openpyxl.load_workbook("1.xlsx")
 worksheet = wookbook.active
-for i in range(0, worksheet.max_row):
-    for col in worksheet.iter_cols(1, worksheet.max_column):
-        print(col[i].value, end="\t\t")
-    print('')
+#print(worksheet[1][0].value)
+for row in range(1,worksheet.max_row+1):
+    city = worksheet[row][0].value
+    number = worksheet[row][1].value
+#    index=city.find(a)
+    print(index)
+    print(city)
+
+
+
+import pandas as pd
+# Load the xlsx file
+excel_data = pd.read_excel('1.xlsx')
+# Read the values of the file in the dataframe
+data = pd.DataFrame(excel_data, columns=['город', 'Телефон'])
+print(data[data=='1'])
+#print(data.loc[data['город']=='1'])
+# Print the content
+#print("The content of the file is:\n", data)
+#print(data.loc)
+#data[(['Город'] == 'Пермь')]
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import openpyxl
-a=("П")
-wookbook = openpyxl.load_workbook("1.xlsx")
-worksheet = wookbook.active
-#print(worksheet[1][0].value)
-for row in range(1,worksheet.max_row+1):
-    city = worksheet[row][0].value
-    number = worksheet[row][1].value
-    index=city.find(a)
-    print(index)
-    print(city)
+import pandas as pd
+a=(input('введите город'))
+df = pd.read_excel('1.xlsx')
+#print(df['город'])
+print(df[df['город']== a ])
+
+#print(data[data=='1'])
+#print(data.loc[data['город']=='1'])
+# Print the content
+#print("The content of the file is:\n", data)
+#print(data.loc)
+#data[(['Город'] == 'Пермь')]
